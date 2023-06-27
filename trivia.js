2385ae733166611c8a841b5cb26b61fb557bbf31
georgesroberto <georgesroberto21@gmail.com>
2023-06-27 19:29:05 +0300

    Bible Trivia Day 1

---
+++ trivia.js
@@ -0,0 +1,94 @@
+const trivia = [
+    {    
+        question: "According to the Bible, who was swallowed by a great fish?",
+            a: "Jonah",
+            b: "Moses",
+            c: "David",
+            d: "Noah",
+        correct:"c"
+    },    
+    
+    {    
+        question: "Which book of the Bible contains the Ten Commandments?",
+            a: "Exodus",
+            b: "Genesis",
+            c: "Leviticus",
+            d: "Deuteronomy",
+        correct: "a"
+    },
+
+    {
+        question: "Who was the first king of Israel?",
+            a: "Solomon",
+            b: "Saul",
+            c: "David",
+            d: "Samuel",
+        correct: "b"
+    },
+
+    {
+        question: "Which of the following is not one of the Gospel writers?",
+            a: "Matthew",
+            b: "John",
+            c: "Peter",
+            d: "Mark",
+        correct: "a"
+    },
+    
+    {
+        question: "In the story of the Garden of Eden, what fruit did Adam and Eve eat?",
+        "options": {
+        "a": "Apple",
+        "b": "Pear",
+        "c": "Fig",
+        "d": "Banana"
+        }
+    },
+    {
+        question: "Who was sold into slavery by his brothers in the Bible?",
+        "options": {
+        "a": "Isaac",
+        "b": "Joseph",
+        "c": "Jacob",
+        "d": "Ishmael"
+        }
+    },
+    {
+        question: "Which of the following is not one of the Plagues of Egypt?",
+        "options": {
+        "a": "Frogs",
+        "b": "Locusts",
+        "c": "Hail",
+        "d": "Darkness"
+        }
+    },
+    {
+        question: "How many disciples did Jesus have?",
+        "options": {
+        "a": "10",
+        "b": "12",
+        "c": "15",
+        "d": "20"
+        }
+    },
+    {
+        question: "Who betrayed Jesus to the authorities?",
+        "options": {
+        "a": "Peter",
+        "b": "James",
+        "c": "Judas",
+        "d": "John"
+        }
+    },
+    {
+        question: "What is the first book of the New Testament?",
+        "options": {
+        "a": "Matthew",
+        "b": "Luke",
+        "c": "Romans",
+        "d": "Acts"
+        },
+        "correct": "c"
+    }
+      
+];
